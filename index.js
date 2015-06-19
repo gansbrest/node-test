c71249cb52eff3df4e2bc0528adca902300462d2
Sergey Khaladzinski <gansbrest@gmail.com>
2015-06-19 19:16:27 -0400

    Sample node app

---
+++ index.js
@@ -0,0 +1,19 @@
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection({ 
+    port: 8888
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello', 
+    handler: function (request, reply) {
+       reply('hello world');
+    }
+});
+
+// Start the server
+server.start();
